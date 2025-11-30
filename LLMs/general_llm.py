56a03a370f2b55d944655aa2e6312b3a3b2b0481
AdrianGras <adriangraslopez@gmail.com>
2025-11-30 13:21:43 +0100

    Completed GeneralLLM implementation

--- LLMs/general_llm.py
+++ LLMs/general_llm.py
@@ -1,10 +1,12 @@
 from abc import ABC, abstractmethod
 import os
+import json
 
 class GeneralLLM(ABC):
-    def __init__(self):
+    def __init__(self, system_prompt_id = "default"):
         super().__init__()
-        self.api_key = self.load_api_credentials()
+        api_key = self.load_api_credentials()
+        self.client = self.generate_client(api_key)
 
 
     def load_api_credentials(self):
@@ -15,7 +17,7 @@ class GeneralLLM(ABC):
         if not api_key:
             raise ValueError(f"API key for {self.api_key_name} not found in environment variables.")
         return api_key
-
+    
     @abstractmethod
     @property
     def api_key_name(self):
@@ -24,4 +26,16 @@ class GeneralLLM(ABC):
         """
         pass
 
-    
+    @abstractmethod
+    def generate_client(self, api_key):
+        """
+        Generates and returns the LLM client using the provided API key.
+        """
+        pass
+
+    @abstractmethod
+    def chat_completion(self, message_history, system_prompt, temperature=0.7, max_tokens=150):
+        """
+        Generates a chat completion based on the message history and system prompt.
+        """
+        pass
